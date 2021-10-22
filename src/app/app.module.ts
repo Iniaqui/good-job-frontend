b339228254ead70ee3c83d97c7c0f0dcdc21ec30
Maurel CODO <maurelcodo@gmail.com>
2021-10-22 12:02:00 +0200

    [GOOD-1] : Initialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { MenuComponent } from './menu/menu.component';
+import { DecouvrirComponent } from './decouvrir/decouvrir.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
+const appRoutes: Routes =[
+{path:"acceuil", component:DecouvrirComponent}
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    MenuComponent,
+    DecouvrirComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
