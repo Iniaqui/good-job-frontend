0dc623f21994d9b55913715bfa89d04924ff7d7f
Maurel CODO <maurelcodo@gmail.com>
2021-10-24 12:31:55 +0200

    Page de connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,20 @@ import { MenuComponent } from './menu/menu.component';
 import { DecouvrirComponent } from './decouvrir/decouvrir.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 const appRoutes: Routes =[
-{path:"acceuil", component:DecouvrirComponent}
+{path:"acceuil", component:DecouvrirComponent},
+{path:"connexion", component:ConnexionComponent}
 
 ]
 @NgModule({
@@ -20,11 +31,21 @@ const appRoutes: Routes =[
     MenuComponent,
     DecouvrirComponent,
     FooterComponent,
+    ConnexionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
